8f291f05582f665b88a11da5a08d0a71822db558
Brauckhoff <121032772+Brauckhoff@users.noreply.github.com>
2023-11-27 15:10:50 +0100

    Add files via upload

---
+++ R/connectoR.R
@@ -0,0 +1,29 @@
+#' Pixel Interpolation
+#'
+#' Connects to points in a matrix, array or an image
+#' @param row1 first row: together with col1 coordinate for the first point
+#' @param col1 first column: together with row1 coordinate for the first point
+#' @param row2 second column: together with col2 coordinate for the second point
+#' @param col2 second column: together with row2 coordinate for the second point
+#' @returns
+#' data frame containing the coordinates to connect the two input points
+#' @examples
+#' test <- matrix(0, 4, 4)
+#' test[1, 1] <- 1
+#' test[3, 4] <- 1
+#' link <- connectoR(1, 1, 3, 4)
+#' test[link] <- 1
+#' @export
+connectoR <- function(row1, col1, row2, col2) {
+  # calculate the number of points needed for interpolation
+  num_points <- max(abs(row2 - row1), abs(col2 - col1)) + 1
+
+  # generate linearly spaced coordinates
+  rows <- round(seq(row1, row2, length.out = num_points))
+  cols <- round(seq(col1, col2, length.out = num_points))
+
+  # combine row and column coordinates into a matrix
+  interpolated_pixels <- cbind(rows, cols)
+
+  return(interpolated_pixels)
+}
---
+++ R/vis_linkeR.R
@@ -0,0 +1,43 @@
+#' Visualization for the linkeR function
+#'
+#' Can be generally used to change color of specified pixels in an image. In
+#' this Package it is meant to visualize and highlight the pixels that where
+#' added by the linkeR function.
+#' @param image image
+#' @param coord Coordinates specifying which pixels to be colored (should
+#' be a X|Y Data frame)
+#' @param color Color with which to replace specified pixels. Can be either a
+#' an RGB triplet or one of the colors listed by \code{\link[grDevices]{colors}}.
+#' @returns
+#' plot displaying the image with highlighted pixels at desired coordinates
+#' @examples
+#' to do
+#' @references https://CRAN.R-project.org/package=countcolors
+#' @export
+vis_linkeR <- function(image, coords, color = "green") {
+
+  # adapted function: changePixelColor from countcolors
+  # transform image to array so that transformation of pixels becomes available
+  img_array <- as.array(image)
+
+  # Make sure target color is or can be coerced to an RGB triplet
+  if (is.character(color)) {
+    color <- as.vector(col2rgb(color) / 255)
+  }
+
+  # check that vector is of appropriate length and has a 0-1 (not 0-255) range
+  if (length(color) != 3) {
+    stop("'color' must be a numeric vector of length 3 with
+             values between 0 and 1 or one of the colors listed by colors()")
+  } else if (range(color)[2] > 1) {
+    color <- color / 255
+  }
+
+  # change specified pixels to target color
+  for (i in 1:nrow(coords)) {
+    img_array[coords[i, 1], coords[i, 2], 1, 1:3] <- color
+  }
+
+  # display result
+  as.cimg(img_array) |> plot()
+}
