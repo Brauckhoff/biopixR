2f589b4e54b6a7927026d4fa32f18292e49061ee
Brauckhoff <121032772+Brauckhoff@users.noreply.github.com>
2023-11-29 07:16:49 +0100

    Add files via upload

---
+++ R/interactive_detectoR.R
@@ -0,0 +1,170 @@
+#' Interactive detecting of beads
+#'
+#' This function uses the vis_detectoR function to visualize the detected beads
+#' at varying threshold an smoothing parameters.
+#' @param image image (preferred import: \code{\link[imager]{load.image}})
+#' @param resolution description
+#' @param return_param Should the final variables for alpha and sigma should be
+#' printed in the console (TRUE or FALSE).
+#' @references https://CRAN.R-project.org/package=magickGUI
+#' @examples
+#' interactive_detectoR(beads)
+#' @export
+interactive_detectoR <-
+  function(image,
+           resolution = 0.1,
+           return_param = FALSE) {
+    # assign imports
+    image_original <- cimg2magick(image)
+    beads <- image
+
+    # make initial input
+    alpha <- 0.75
+    sigma <- 0.1
+    initial_cimg <- vis_detectoR(beads, alpha, sigma)
+    initial <- cimg2magick(initial_cimg$marked_beads)
+
+    # set variable range
+    range_alpha <- c(0, 3)
+    range_sigma <- c(0, 3)
+    length_slider <- as.integer(nrow(beads) * 0.6)
+    if (length_slider < 200) {
+      length_slider <- 200
+    }
+    text_label_alpha <- "Alpha/Threshold:"
+    text_label_sigma <- "Sigma/Smoothing:"
+    quit_waiting <- !is.null(getOption("unit_test_magickGUI"))
+    temp <- tempfile(fileext = ".jpg")
+    on.exit(unlink(temp), add = TRUE)
+    image_write(initial, temp)
+    image_tcl <- tkimage.create("photo", "image_tcl", file = temp)
+    label_digits <- -as.integer(log(resolution, 10))
+    label_digits <- ifelse(label_digits > 0, label_digits, 0)
+    label_template <- sprintf("%%.%df", label_digits)
+
+    # configure widgets
+    win1 <- tktoplevel()
+    on.exit(tkdestroy(win1), add = TRUE)
+    win1.frame1 <- tkframe(win1)
+    win1.frame2 <- tkframe(win1)
+    win1.im <- tklabel(win1, image = image_tcl)
+    win1.frame1.label <-
+      tklabel(win1.frame1, text = sprintf("%s%s", text_label_alpha, sprintf(label_template, alpha)))
+    win1.frame2.label <-
+      tklabel(win1.frame2, text = sprintf("%s%s", text_label_sigma, sprintf(label_template, sigma)))
+    slider_value_alpha <- tclVar(alpha)
+    slider_value_sigma <- tclVar(sigma)
+    command_slider_alpha <- function(...)
+    {
+      assign("slider_value_radius", slider_value_alpha, inherits = TRUE)
+    }
+    command_slider_sigma <- function(...)
+    {
+      assign("slider_value_sigma", slider_value_sigma, inherits = TRUE)
+    }
+
+    win1.frame1.slider <-
+      tkscale(
+        win1.frame1,
+        from = range_alpha[1],
+        to = range_alpha[2],
+        variable = slider_value_alpha,
+        orient = "horizontal",
+        length = length_slider,
+        command = command_slider_alpha,
+        resolution = resolution,
+        showvalue = 0
+      )
+    win1.frame2.slider <-
+      tkscale(
+        win1.frame2,
+        from = range_sigma[1],
+        to = range_sigma[2],
+        variable = slider_value_sigma,
+        orient = "horizontal",
+        length = length_slider,
+        command = command_slider_sigma,
+        resolution = resolution,
+        showvalue = 0
+      )
+    temp_val <- c(alpha, sigma)
+    update_image <- function()
+    {
+      temp_image <- vis_detectoR(beads, temp_val[1], temp_val[2])
+      temp_image <- cimg2magick(temp_image$marked_beads)
+      image_write(temp_image, temp)
+      image_tcl <- tkimage.create("photo", "image_tcl", file = temp)
+      tkconfigure(win1.im, image = image_tcl)
+    }
+    command_button <- function(...)
+    {
+      assign("quit_waiting", TRUE, inherits = TRUE)
+    }
+
+    win1.button <-
+      tkbutton(win1, text = "OK", command = command_button)
+    tkpack(win1.im, side = "top")
+    tkpack(win1.frame1.label, side = "left", anchor = "c")
+    tkpack(win1.frame1.slider, side = "left", anchor = "c")
+    tkpack(win1.frame1, side = "top", anchor = "c")
+    tkpack(win1.frame2.label, side = "left", anchor = "c")
+    tkpack(win1.frame2.slider, side = "left", anchor = "c")
+    tkpack(win1.frame2, side = "top", anchor = "c")
+    tkpack(win1.button,
+           side = "top",
+           anchor = "c",
+           pady = 20)
+    pre_slider_values <-
+      c(as.numeric(tclvalue(slider_value_alpha)), as.numeric(tclvalue(slider_value_sigma)))
+    if (quit_waiting)
+    {
+      wait_test <- TRUE
+      while (wait_test)
+      {
+        wait_test <- FALSE
+        tryCatch({
+          tkwm.state(win1)
+        },
+        error = function(e)
+          assign("wait_test", TRUE, inherits = TRUE))
+      }
+      wait_time_long()
+      tkdestroy(win1.button)
+    }
+    tkwm.state(win1, "normal")
+    while (TRUE)
+    {
+      tryCatch({
+        tkwm.state(win1)
+      },
+      error = function(e)
+        assign("quit_waiting", TRUE, inherits = TRUE))
+      if (quit_waiting)
+        break
+      temp_val <-
+        c(as.numeric(tclvalue(slider_value_alpha)), as.numeric(tclvalue(slider_value_sigma)))
+      if (any(temp_val != pre_slider_values))
+      {
+        temp_label_alpha <-
+          sprintf("%s%s",
+                  text_label_alpha,
+                  sprintf(label_template, temp_val[1]))
+        temp_label_sigma <-
+          sprintf("%s%s",
+                  text_label_sigma,
+                  sprintf(label_template, temp_val[2]))
+        tkconfigure(win1.frame1.label, text = temp_label_alpha)
+        tkconfigure(win1.frame2.label, text = temp_label_sigma)
+        update_image()
+        pre_slider_values <- temp_val
+      }
+    }
+    val_res <- pre_slider_values
+    names(val_res) <- c("alpha", "sigma")
+    if (return_param)
+    {
+      return(val_res)
+    }
+    out <- list(val_res)
+    out
+  }
--- R/linkeR.R
+++ R/linkeR.R
@@ -10,6 +10,9 @@
 #' @import imager
 #' @import magick
 #' @import datatable
+#' @details
+#' Additional details...
+#'
 #' @examples
 #' # example code
 #'
@@ -20,7 +23,8 @@ linkeR <-
            droplet.img,
            threshold = "13%",
            alpha = 0.75,
-           sigma = 0.1) {
+           sigma = 0.1,
+           visualize = TRUE) {
     # assign imports
     beads_to_del <- bead.img
     droplets <- droplet.img
@@ -61,7 +65,8 @@ linkeR <-
     end_points_df <- as.data.frame(end_points)
     colnames(end_points_df) <- c("x", "y", "dim3", "dim4")
 
-    diagonal_edges <- which(diagonalends_cimg == TRUE, arr.ind = TRUE)
+    diagonal_edges <-
+      which(diagonalends_cimg == TRUE, arr.ind = TRUE)
     diagonal_edges_df <- as.data.frame(diagonal_edges)
     colnames(diagonal_edges_df) <- c("x", "y", "dim3", "dim4")
 
@@ -88,15 +93,99 @@ linkeR <-
       value = unlist(alt_value)
     )
 
-    #
+    # scan the surroundings of line ends and connect with nearest cluster
+    # creates a binary matrix that can be displayed as image
+    # (0 = background/black & 1 = white/foreground)
     first_overlay <-
       scanneR(end_points_df,
               diagonal_edges_df,
               clean_lab_df,
               lineends_cimg)
 
+    # combine matrix with starting image
     first_connect <-
       parmax(list(thresh_clean_cimg, as.cimg(first_overlay$overlay)))
 
+    # repeat above process:
+    # line end and diagonal end calculation
+    connect_magick <- cimg2magick(first_connect)
+    thin_connect <-
+      image_morphology(connect_magick, "thinning", "skeleton")
+    mo3_lineends <-
+      image_morphology(thin_connect, "HitAndMiss", "LineEnds")
+    mo4_diagonalends <-
+      image_morphology(thin_connect, "HitAndMiss", "LineEnds:2>")
 
+    thin_cimg <- magick2cimg(thin_connect)
+    sec_lineends_cimg <- magick2cimg(mo3_lineends)
+    sec_diagonalends_cimg <- magick2cimg(mo4_diagonalends)
+
+    # label image
+    sec_lab <- label(thin_cimg)
+    df_sec_lab <- as.data.frame(sec_lab) |>
+      subset(value > 0)
+
+    # transform extracted points into data.frames
+    sec_lineends <- which(sec_lineends_cimg == TRUE, arr.ind = TRUE)
+    sec_lineends_df <- as.data.frame(sec_lineends)
+    colnames(sec_lineends_df) <- c("x", "y", "dim3", "dim4")
+
+    sec_diagonal_edges <-
+      which(sec_diagonalends_cimg == TRUE, arr.ind = TRUE)
+    sec_diagonal_edges_df <- as.data.frame(sec_diagonal_edges)
+    colnames(sec_diagonal_edges_df) <- c("x", "y", "dim3", "dim4")
+
+    # more important to exclude closed circles then in the first run, as now
+    # more partitions have continuous edges
+    lab2_clean_x <- list()
+    ĺab2_clean_y <- list()
+    lab2_clean_value <- list()
+
+    for (e in 1:nrow(df_sec_lab)) {
+      if (thin_cimg[df_sec_lab[e, 1], df_sec_lab[e, 2]] == 1) {
+        lab2_clean_x[e] <- df_sec_lab[e, ]$x
+        ĺab2_clean_y[e] <- df_sec_lab[e, ]$y
+        lab2_clean_value[e] <- df_sec_lab[e, ]$value
+      }
+    }
+
+    df_sec_lab_clean <- data.frame(
+      x = unlist(lab2_clean_x),
+      y = unlist(ĺab2_clean_y),
+      value = unlist(lab2_clean_value)
+    )
+
+    # second time filling up gaps in lines
+    sec_overlay <-
+      scanneR(sec_lineends_df,
+              sec_diagonal_edges_df,
+              df_sec_lab_clean,
+              thin_cimg)
+
+    # combine overlay matrix with former image
+    result <- parmax(list(thin_cimg, sec_overlay$overlay))
+
+    # final thin edges
+    result_magick <- cimg2magick(result)
+    result_thin <-
+      image_morphology(result_magick, "Thinning", "Skeleton")
+    result_cimg <- magick2cimg(result_thin)
+
+    if (visualize = TRUE) {
+      # creating an cimg in which there are 0, 1 and 2
+      # 0 - background
+      # 1 - filled by algorithm
+      # 2 - original line
+      vis <- add(list(result_cimg, neg_thresh_m))
+      vis_col <- add.color(vis)
+
+      # get coordinates from new / filled pixels
+      to_color <- which(vis_col == 1, arr.ind = TRUE)
+
+      # colorize and plot specific coordinates with vis_linkeR function
+      vis_linkeR(neg_thresh_m, to_color)
+    }
+
+    out <- list(result_cimg)
+    out
   }
