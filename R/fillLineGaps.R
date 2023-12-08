30f813dbd37af3861b1cc8b155ce83f06ff69d8b
Brauckhoff <Tim.Brauckhoff@b-tu.de>
2023-12-08 13:36:04 +0100

    merged fillLineGaps

--- R/fillLineGaps.R
+++ R/fillLineGaps.R
@@ -11,6 +11,8 @@
 #' (from \code{\link[imager]{cannyEdges}}).
 #' @param sigma smoothing (from \code{\link[imager]{cannyEdges}}).
 #' @param radius maximal radius that should be scanned for another cluster
+#' @param iterations how many times the algorithm should find line ends and
+#' reconnect them to their closest neighbor
 #' @param visualize If TRUE (default) a plot is displayed highlighting the
 #' added pixels in the original image.
 #' @returns image with continuous edges (closed gaps)
@@ -38,6 +40,7 @@ fillLineGaps <-
            alpha = 0.75,
            sigma = 0.1,
            radius = 5,
+           iterations = 2,
            visualize = TRUE) {
     # assign import
     droplets <- droplet.img
@@ -54,162 +57,129 @@ fillLineGaps <-
     # discontinuous edges, as otherwise they may connect with the beads)
     # removes objects to prevent reconnecting with labeled regions that
     # are not lines/edges
-    if(!is.null(bead.img)) {
+    if (!is.null(bead.img)) {
       beads_to_del <- bead.img
       bead_coords <- objectDetection(beads_to_del, alpha, sigma)
 
       # transform binary image to array in order to modify individual values
       thresh_array <- as.array(neg_thresh_m)
       for (i in 1:nrow(bead_coords$coordinates)) {
-        thresh_array[
-          bead_coords$coordinates[i, 1],
-          bead_coords$coordinates[i, 2], 1, 1
-        ] <- 0
+        thresh_array[bead_coords$coordinates[i, 1],
+                     bead_coords$coordinates[i, 2], 1, 1] <- 0
       }
       # removed beads from droplets and retransformation to cimg
       thresh_clean_cimg <- as.cimg(thresh_array)
     } else {
-      thresh_clean_cimg <- neg_thresh_cimg
+      thresh_clean_cimg <- neg_thresh_m
     }
 
-    # getting coordinates of all line ends and only diagonal line ends
+    # same orientation for 'cimg' and 'magick-image'
+    thresh_clean_m <- mirror(thresh_clean_cimg, axis = "x")
     thresh_clean_magick <- cimg2magick(thresh_clean_cimg)
-    mo1_lineends <- image_morphology(
-      thresh_clean_magick,
-      "HitAndMiss", "LineEnds"
-    )
-    mo2_diagonalends <- image_morphology(
-      thresh_clean_magick,
-      "HitAndMiss", "LineEnds:2>"
-    )
 
-    # transform extracted coordinates into data frames
-    lineends_cimg <- magick2cimg(mo1_lineends)
-    diagonalends_cimg <- magick2cimg(mo2_diagonalends)
+    for (i in 1:iterations) {
+      # getting coordinates of all line ends and only diagonal line ends
+      mo1_lineends <- image_morphology(thresh_clean_magick,
+                                       "HitAndMiss", "LineEnds")
+      mo2_diagonalends <- image_morphology(thresh_clean_magick,
+                                           "HitAndMiss", "LineEnds:2>")
 
-    end_points <- which(lineends_cimg == TRUE, arr.ind = TRUE)
-    end_points_df <- as.data.frame(end_points)
-    colnames(end_points_df) <- c("x", "y", "dim3", "dim4")
+      # transform extracted coordinates into data frames
+      lineends_cimg <- magick2cimg(mo1_lineends)
+      diagonalends_cimg <- magick2cimg(mo2_diagonalends)
 
-    diagonal_edges <-
-      which(diagonalends_cimg == TRUE, arr.ind = TRUE)
-    diagonal_edges_df <- as.data.frame(diagonal_edges)
-    colnames(diagonal_edges_df) <- c("x", "y", "dim3", "dim4")
+      end_points <- which(lineends_cimg == TRUE, arr.ind = TRUE)
+      end_points_df <- as.data.frame(end_points)
+      colnames(end_points_df) <- c("x", "y", "dim3", "dim4")
 
-    # labeling lines and transforming into data frame
-    lab <- label(neg_thresh_m)
-    df_lab <- as.data.frame(lab) |>
-      subset(value > 0)
+      diagonal_edges <-
+        which(diagonalends_cimg == TRUE, arr.ind = TRUE)
+      diagonal_edges_df <- as.data.frame(diagonal_edges)
+      colnames(diagonal_edges_df) <- c("x", "y", "dim3", "dim4")
 
-    # make sure that only lines are labeled and closed circles are not included
-    # in the data frame, as line ends could connect with them
-    alt_x <- list()
-    alt_y <- list()
-    alt_value <- list()
-    for (g in 1:nrow(df_lab)) {
-      #droplets_array <- as.array(droplets)
-      if (neg_thresh_m[df_lab$x[g], df_lab$y[g], 1, 1] == 1) {
-        alt_x[g] <- df_lab$x[g]
-        alt_y[g] <- df_lab$y[g]
-        alt_value[g] <- df_lab$value[g]
+      # labeling lines and transforming into data frame
+      if (i == 1) {
+        lab <- label(thresh_clean_m)
+        df_lab <- as.data.frame(lab) |>
+          subset(value > 0)
+      } else {
+        lab <- label(out_cimg)
+        df_lab <- as.data.frame(lab) |>
+          subset(value > 0)
       }
-    }
-    clean_lab_df <- data.frame(
-      x = unlist(alt_x),
-      y = unlist(alt_y),
-      value = unlist(alt_value)
-    )
 
-    # scan the surroundings of line ends and connect with nearest cluster
-    # creates a binary matrix that can be displayed as image
-    # (0 = background/black & 1 = white/foreground)
-    first_overlay <-
-      adaptiveInterpolation(
-        end_points_df,
-        diagonal_edges_df,
-        clean_lab_df,
-        lineends_cimg,
-        radius = radius
+      # make sure that only lines are labeled and closed circles are not included
+      # in the data frame, as line ends could connect with them
+      alt_x <- list()
+      alt_y <- list()
+      alt_value <- list()
+
+      if (i == 1) {
+        for (g in 1:nrow(df_lab)) {
+          #droplets_array <- as.array(droplets)
+          if (thresh_clean_m[df_lab$x[g], df_lab$y[g], 1, 1] == 1) {
+            alt_x[g] <- df_lab$x[g]
+            alt_y[g] <- df_lab$y[g]
+            alt_value[g] <- df_lab$value[g]
+          }
+        }
+      } else {
+        for (g in 1:nrow(df_lab)) {
+          #droplets_array <- as.array(droplets)
+          if (out_cimg[df_lab$x[g], df_lab$y[g], 1, 1] == 1) {
+            alt_x[g] <- df_lab$x[g]
+            alt_y[g] <- df_lab$y[g]
+            alt_value[g] <- df_lab$value[g]
+          }
+        }
+      }
+
+      clean_lab_df <- data.frame(
+        x = unlist(alt_x),
+        y = unlist(alt_y),
+        value = unlist(alt_value)
       )
 
-    # combine matrix with starting image
-    first_connect <-
-      parmax(list(neg_thresh_m, as.cimg(first_overlay$overlay)))
+      # scan the surroundings of line ends and connect with nearest cluster
+      # creates a binary matrix that can be displayed as image
+      # (0 = background/black & 1 = white/foreground)
+      first_overlay <-
+        adaptiveInterpolation(end_points_df,
+                              diagonal_edges_df,
+                              clean_lab_df,
+                              lineends_cimg,
+                              radius = radius)
 
-    # repeat above process:
-    # line end and diagonal end calculation
-    connect_magick <- cimg2magick(first_connect)
-    thin_connect <-
-      image_morphology(connect_magick, "thinning", "skeleton")
-    mo3_lineends <-
-      image_morphology(thin_connect, "HitAndMiss", "LineEnds")
-    mo4_diagonalends <-
-      image_morphology(thin_connect, "HitAndMiss", "LineEnds:2>")
-
-    thin_cimg <- magick2cimg(thin_connect)
-    sec_lineends_cimg <- magick2cimg(mo3_lineends)
-    sec_diagonalends_cimg <- magick2cimg(mo4_diagonalends)
-
-    # label image
-    sec_lab <- label(thin_cimg)
-    df_sec_lab <- as.data.frame(sec_lab) |>
-      subset(value > 0)
-
-    # transform extracted points into data.frames
-    sec_lineends <- which(sec_lineends_cimg == TRUE, arr.ind = TRUE)
-    sec_lineends_df <- as.data.frame(sec_lineends)
-    colnames(sec_lineends_df) <- c("x", "y", "dim3", "dim4")
-
-    sec_diagonal_edges <-
-      which(sec_diagonalends_cimg == TRUE, arr.ind = TRUE)
-    sec_diagonal_edges_df <- as.data.frame(sec_diagonal_edges)
-    colnames(sec_diagonal_edges_df) <- c("x", "y", "dim3", "dim4")
-
-    # more important to exclude closed circles then in the first run, as now
-    # more partitions have continuous edges
-    lab2_clean_x <- list()
-    lab2_clean_y <- list()
-    lab2_clean_value <- list()
-
-    for (e in 1:nrow(df_sec_lab)) {
-      if (thin_cimg[df_sec_lab[e, 1], df_sec_lab[e, 2], 1, 1] == 1) {
-        lab2_clean_x[e] <- df_sec_lab[e, ]$x
-        lab2_clean_y[e] <- df_sec_lab[e, ]$y
-        lab2_clean_value[e] <- df_sec_lab[e, ]$value
+      # combine matrix with starting image
+      if (i == 1) {
+        first_connect <-
+          parmax(list(thresh_clean_m, as.cimg(first_overlay$overlay)))
+      } else {
+        first_connect <-
+          parmax(list(out_cimg, as.cimg(first_overlay$overlay)))
       }
+
+      first_connect_m <- mirror(first_connect, axis = "x")
+
+      # repeat above process:
+      # line end and diagonal end calculation
+      connect_magick <- cimg2magick(first_connect_m)
+      thresh_clean_magick <-
+        image_morphology(connect_magick, "thinning", "skeleton")
+
+      out_cimg <- magick2cimg(thresh_clean_magick)
     }
 
-    df_sec_lab_clean <- data.frame(
-      x = unlist(lab2_clean_x),
-      y = unlist(lab2_clean_y),
-      value = unlist(lab2_clean_value)
-    )
-
-    # second time filling up gaps in lines
-    sec_overlay <-
-      adaptiveInterpolation(
-        sec_lineends_df,
-        sec_diagonal_edges_df,
-        df_sec_lab_clean,
-        thin_cimg,
-        radius = radius
-      )
-
-    # combine overlay matrix with former image
-    result <- parmax(list(thin_cimg, as.cimg(sec_overlay$overlay)))
-
-    # final thin edges
-    result_magick <- cimg2magick(result)
-    result_thin <-
-      image_morphology(result_magick, "Thinning", "Skeleton")
-    result_cimg <- magick2cimg(result_thin)
+    # resulting image with continous edges
+    result_cimg <- magick2cimg(thresh_clean_magick)
+    result_m <- mirror(result_cimg, axis = "x")
 
     if (visualize == TRUE) {
       # creating an cimg in which there are 0, 1 and 2
       # 0 - background
       # 1 - filled by algorithm
       # 2 - original line
-      vis <- add(list(result_cimg, neg_thresh_m))
+      vis <- add(list(result_m, thresh_clean_cimg))
       vis_col <- add.color(vis)
 
       # get coordinates from new / filled pixels
@@ -219,6 +189,6 @@ fillLineGaps <-
       changePixelColor(neg_thresh_m, to_color)
     }
 
-    out <- result_cimg
+    out <- result_m
     out
   }
